ef1a65124b796526a2bb8ebe6dd85dbffa404a28
PazdzierzA <adampaz696@student.polsl.pl>
2025-01-14 22:25:59 +0100

    compress i decompress
    funkcje compress i decompress

--- ans_library.py
+++ ans_library.py
@@ -3,17 +3,21 @@ import zstandard as zstd
 from read_file import read_text_from_file
 
 # Przykładowe dane
-text = read_text_from_file('data/short/KubusPuchatek/Polish.txt')
+def compress_data(file):
+    text = read_text_from_file(file)
 
-# Konwersja tekstu na bajty
-data = text.encode('utf-8')
+    # Konwersja tekstu na bajty
+    data = text.encode('utf-8')
 
-# Kompresja
-cctx = zstd.ZstdCompressor()
-compressed_data = cctx.compress(data)
-print("Skompresowane dane:", compressed_data)
+    # Kompresja
+    cctx = zstd.ZstdCompressor()
+    compressed_data = cctx.compress(data)
+    return compressed_data
+    print("Skompresowane dane:", compressed_data)
 
-# Dekompresja
-dctx = zstd.ZstdDecompressor()
-decompressed_data = dctx.decompress(compressed_data)
-print("Odkodowane dane:", decompressed_data.decode('utf-8'))
+def decompress_data(compressed_data):
+    # Dekompresja
+    dctx = zstd.ZstdDecompressor()
+    decompressed_data = dctx.decompress(compressed_data)
+    return decompressed_data
+    print("Odkodowane dane:", decompressed_data.decode('utf-8'))

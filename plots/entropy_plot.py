9f59c6b0e7300fc637cca75560275835bc378206
PazdzierzA <adampaz696@student.polsl.pl>
2025-01-15 00:37:08 +0100

    plot entropy
    plot entropy

--- plots/entropy_plot.py
+++ plots/entropy_plot.py
@@ -3,10 +3,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 matplotlib.use('TkAgg')
 
-def entropy_plot_func():
+
+def entropy_plot_func(polish_value, english_value, french_value,hungarian_value ):
+
     # Dane przykładowe: entropia dla 4 języków
     languages = ["Polish", "English", "French", "Hungarian"]
-    entropy_values = [4.5, 4.3, 4.6, 4.4]  # Przykładowe wartości entropii
+    entropy_values = [polish_value,english_value, french_value, hungarian_value]  # Przykładowe wartości entropii
     
     # Tworzenie wykresu
     plt.figure(figsize=(8, 6))  # Rozmiar wykresu
@@ -14,11 +16,12 @@ def entropy_plot_func():
     
     # Opisy osi i tytuł
     plt.xlabel("Language", fontsize=16)
-    plt.ylabel("Entropy value", fontsize=16)
+    plt.ylabel("Entropy value",  fontsize=16)
     plt.title("Entropy in every language", fontsize=16)
     
     # Wyświetlenie wykresu
     plt.grid(axis='y', linestyle='--', alpha=0.7)
     plt.tight_layout()
     plt.show()
+
 
